5d767d165f6931d7b64f91bfd5320f4d88a2951a
31471730 <31471730@099493-P33S102.macklabs.br>
2016-04-04 09:39:12 -0300

    versao 0.1 - 04/04/2016

---
+++ src/subsequenciamaxima/SubsequenciaMaxima.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package subsequenciamaxima;
+
+/**
+ *
+ * @author 31471730
+ */
+public class SubsequenciaMaxima {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static final int A[] = {1, 2, 3, 9, 4, 5, 6};
+
+    public static void main(String[] args) {
+        int s[] = new int[A.length];
+        for (int i = 0; i < s.length; i++) {
+            s[i] = 0;
+        }
+        subsequenciaMaxima(0, 0, s);
+    }
+
+    public static void subsequenciaMaxima(int pos, int posSol, int s[]) {
+        if (posSol == 0) {
+            s[posSol++] = A[pos];
+            pos++;
+        } else if (s[posSol - 1] < A[pos]) {
+            s[posSol++] = A[pos];
+            pos++;
+        } else {
+            pos++;
+            subsequenciaMaxima(pos, posSol - 1, s);
+        }
+        if (pos < A.length) {
+            subsequenciaMaxima(pos, posSol, s);
+        } else {
+            print(s);
+        }
+    }
+
+    public static void print(int[] v) {
+        for (int i : v) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+    }
+
+}
