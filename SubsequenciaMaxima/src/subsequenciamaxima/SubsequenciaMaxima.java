51cfe2d63d8df4b62e04082a1cb863fa62b7b838
916003 <916003@PTINBR-PC001749.ptin.corpPT.com>
2016-04-04 10:42:05 -0300

    versao 0.5 - 04/04/2016

--- SubsequenciaMaxima/src/subsequenciamaxima/SubsequenciaMaxima.java
+++ SubsequenciaMaxima/src/subsequenciamaxima/SubsequenciaMaxima.java
@@ -14,39 +14,47 @@ public class SubsequenciaMaxima {
     /**
      * @param args the command line arguments
      */
-    private static final int A[] = {1, 2, 3, 9, 4, 5, 6};
+    private static final int A[] = {8, 5, 4, 2, 6, 9, 7};
 
     public static void main(String[] args) {
         int s[] = new int[A.length];
         for (int i = 0; i < s.length; i++) {
             s[i] = 0;
         }
-        subsequenciaMaxima(0, 0, s);
+        subsequenciaMaxima(0, 0, s, 0);
     }
 
-    public static void subsequenciaMaxima(int pos, int posSol, int s[]) {
-        if (posSol == 0) {
-            s[posSol++] = A[pos];
-            pos++;
-        } else if (s[posSol - 1] < A[pos]) {
-            s[posSol++] = A[pos];
-            pos++;
-        } else {
-            pos++;
-            subsequenciaMaxima(pos, posSol - 1, s);
-        }
-        if (pos < A.length) {
-            subsequenciaMaxima(pos, posSol, s);
-        } else {
-            print(s);
-        }
+    public static void subsequenciaMaxima(int pos, int posSol, int s[], int i) {
+        int n = 0, posAux;
+        do {
+            posAux = next(A, n, pos);
+            if (posAux != -1) {
+                pos = posAux;
+                s[posSol++] = A[pos];
+                n = A[pos];
+            } else if (pos < A.length) {
+                print(s, posSol);
+                subsequenciaMaxima(++pos, posSol - 1, s, i);
+                pos = -1;
+            }else{
+                break;
+            }
+        } while (pos != -1);
     }
 
-    public static void print(int[] v) {
-        for (int i : v) {
-            System.out.print(i + " ");
+    public static void print(int[] v, int lastPos) {
+        for (int i = 0; i < lastPos; i++) {
+            System.out.print(v[i] + " ");
         }
         System.out.println("");
+    }
+
+    public static int next(int[] v, int n, int pos) {
+        for (int i = pos; i < v.length; i++) {
+            if (v[i] > n)
+                return i;
+        }
+        return -1;
     }
 
 }
